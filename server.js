e2452b6fca2d50b7a4d01aec70d4a3101cf55539
DadhaniyaSomin <somindadhaniya111@gmail.com>
2022-08-07 09:03:58 +0530

    commit added

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
+const {logger} = require('./middleware/logEvents');
+const cors = require('cors');
+
+//custom middleware
+app.use(logger);
+
+//cors 
+app.use(cors());
 
 //build in middleware // handle form data
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'/public')));
 
+
+//routes
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -18,38 +28,19 @@ app.get('/new-page(.html)?', (req, res) => {
 });
 
 app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
-
-// Route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
+    
 });
 
 
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
 
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
-
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html'))
+    {
+        res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    }else if(req.accepts('json')){
+        res.json("404 not found");
+    }
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, ( ) => console.log(`Server running on port ${PORT}`));
